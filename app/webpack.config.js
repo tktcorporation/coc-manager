2b84f80977f424799dadf471b35bbd6addec6b96
tkt <tktcorporation.go@gmail.com>
2020-04-05 04:49:54 +0900

    resolve webpack error

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,15 +1,19 @@
 const path = require("path");
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: path.resolve(__dirname, "src/index.ts"),
     output: {
         path: path.resolve(__dirname, "dist/"),
         filename: "index.js",
         libraryTarget: "umd"
     },
+    externals: {
+        typeorm: "commonjs typeorm"
+    },
     resolve: {
-        extensions: [".ts"],
+        extensions: [".ts", ".js"],
+        modules: [path.resolve(__dirname, "src"), "node_modules"],
         alias: {
             "@src": path.resolve(__dirname, "src")
         }
