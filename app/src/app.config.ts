b71471de92e3522cd2ab44eb3c0d784b3bb49449
tkt <tktcorporation.go@gmail.com>
2020-09-23 08:04:00 +0900

    fix a bug of env vars setting

--- app/src/app.config.ts
+++ app/src/app.config.ts
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 export namespace Config {
-    if (process.env.DB_HOST === undefined) {
+    if (!process.env.DB_HOST) {
         throw new Error("DB_HOST must be set.");
     }
     if (!process.env.DB_PORT) {
@@ -13,7 +13,7 @@ export namespace Config {
     if (!process.env.DB_PASSWORD) {
         throw new Error("DB_PASSWORD must be set.");
     }
-    if (!process.env.DB_DATABASE_NAME) {
+    if (!process.env.DB_DATABASE) {
         throw new Error("DB_DATABASE_NAME must be set.");
     }
     if (!process.env.NODE_ENV) {
@@ -29,7 +29,7 @@ export namespace Config {
     export const DB_PORT: string = process.env.DB_PORT;
     export const DB_USER: string = process.env.DB_USER;
     export const DB_PASSWORD: string = process.env.DB_PASSWORD;
-    export const DB_DATABASE: string = process.env.DB_DATABASE_NAME;
+    export const DB_DATABASE: string = process.env.DB_DATABASE;
     export const NODE_ENV: string = process.env.NODE_ENV;
     export const DB_TZ: string = process.env.DB_TZ;
     export const LINE_NOTIFY_API_TOKEN: string =
