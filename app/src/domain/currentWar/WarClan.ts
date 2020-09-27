9806a6e7411de0652f93727db25ab0c0fed30aae
tkt <tktcorporation.go@gmail.com>
2020-09-28 03:19:27 +0900

    update a domain

--- app/src/domain/currentWar/WarClan.ts
+++ app/src/domain/currentWar/WarClan.ts
@@ -3,14 +3,14 @@ import { ClanTag } from "../ClanTag";
 
 export class WarClan {
     constructor(
-        private destructionPercentage: {},
-        private tag: ClanTag,
-        private name: string,
+        private destructionPercentage: {} | undefined,
+        private tag: ClanTag | undefined,
+        private name: string | undefined,
         private badgeUrls: {},
         private clanLevel: number,
         private attacks: number,
         private stars: number,
-        private expEarned: number,
-        public readonly members: WarMember[]
+        private expEarned: number | undefined,
+        public readonly members: WarMember[] | undefined
     ) {}
 }
